13bd1d8fa45a483a4befdecb73f8203362fc27a0
Steve Shreeve <steve.shreeve@gmail.com>
2019-05-19 11:56:38 -0600

    Initial app

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+coffee
+  export count = 100
+  export name = null
+
+  # add 10 for every click
+  click = -> count += 10
+
+  # add 100 every 5 seconds
+  interval = setInterval () =>
+    count += 100
+  , 5000
+
+stylus
+  p
+    color: blue
+
+pug
+  button(on:click="{click}")
+    | Clicked {count} {count === 1 ? 'time' : 'times'}
+  p Hello, {name}!
