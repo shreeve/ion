e3d211cea03f7366f857e5b1d81a4097807097fd
Steve Shreeve <steve.shreeve@gmail.com>
2019-06-28 04:32:34 -0600

    Use updated destiny operator and verify it

--- src/App.svelte
+++ src/App.svelte
@@ -5,21 +5,26 @@ coffee
 
   # add 10 for every click
   click = -> count += 10
-  foo ≈ count + 10000 # can also use "$: foo = ..." #!# NOTE: no need for COMPUTED VALUES!!!
+  foo « count + 10000 # can also use "$: foo = ..." #!# NOTE: no need for COMPUTED VALUES!!!
 
   # add 100 every 3 seconds
   interval = setInterval () =>
     count += 100
   , 3000
 
-stylus
-  p
-    color: blue
-  h5
-    color: red
-
 pug
   button(on:click="{click}")
     | Clicked {count} {count === 1 ? 'time' : 'times'}
   p Hello, {name}!
   h5 Foo is now... {foo}! Can you even believe it?
+
+  +if('foo > 10600')
+    h5 Now you blew it...
+    +else()
+      h3 You still have a chance! Currently at {foo}
+
+stylus
+  p
+   color: blue
+  h5
+    color: red
